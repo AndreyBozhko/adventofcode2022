c91e69f8f8982873cf0f729f83a4962e6a0b084d
Andrey Bozhko <andybozhko@gmail.com>
2022-12-13 01:54:48 +0000

    simplify

--- day12/day12.go
+++ day12/day12.go
@@ -2,47 +2,20 @@ package main
 
 import (
 	"adventofcode2022/utils"
-	"container/heap"
 	"math"
 	"strconv"
 	"strings"
 )
 
 type Point struct {
-	X, Y, Steps int
-}
-
-type Heap []Point
-
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i].Steps < h[j].Steps }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *Heap) Push(x any) {
-	*h = append(*h, x.(Point))
-}
-
-func (h *Heap) Pop() any {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
+	X, Y int
 }
 
 func solve(lines []string, isStart func(c rune) bool) int {
 	input := make([]string, 0, len(lines))
 	input = append(input, lines...)
 
-	queue := Heap(make([]Point, 0))
-	for x, line := range input {
-		for y, c := range line {
-			if isStart(c) {
-				heap.Push(&queue, Point{X: x, Y: y, Steps: 0})
-			}
-		}
-	}
-
+	queue1, queue2 := make([]Point, 0), make([]Point, 0)
 	visited := make(map[Point]bool)
 
 	var destX, destY int
@@ -50,14 +23,16 @@ func solve(lines []string, isStart func(c rune) bool) int {
 	steps := make([][]int, len(input))
 	for x, line := range input {
 		steps[x] = make([]int, len(line))
-		for idx := 0; idx < len(line); idx++ {
-			steps[x][idx] = math.MaxInt32
+
+		for y, c := range line {
+			steps[x][y] = math.MaxInt32
+			if isStart(c) {
+				steps[x][y] = 0
+				queue1 = append(queue1, Point{X: x, Y: y})
+			}
 		}
 
-		if y := strings.Index(line, "S"); y != -1 {
-			steps[x][y] = 0
-			input[x] = strings.ReplaceAll(input[x], "S", "a")
-		}
+		input[x] = strings.ReplaceAll(input[x], "S", "a")
 
 		if y := strings.Index(line, "E"); y != -1 {
 			destX, destY = x, y
@@ -85,46 +60,40 @@ func solve(lines []string, isStart func(c rune) bool) int {
 		return
 	}
 
-	for len(queue) > 0 {
+	for len(queue1) > 0 {
 
-		p := heap.Pop(&queue).(Point)
+		for _, p := range queue1 {
 
-		if _, ok := visited[Point{X: p.X, Y: p.Y}]; !ok {
-			steps[p.X][p.Y] = p.Steps
-			visited[Point{X: p.X, Y: p.Y}] = true
-		} else {
-			continue
-		}
+			if visited[p] {
+				continue
+			}
 
-		if p.X == destX && p.Y == destY {
-			return p.Steps
-		}
+			visited[p] = true
 
-		for _, n := range neighbors(p) {
-			stepCondition := steps[n.X][n.Y] > p.Steps+1
+			if p.X == destX && p.Y == destY {
+				return steps[p.X][p.Y]
+			}
 
-			to, from := input[n.X][n.Y], input[p.X][p.Y]
-			heightCondition := to <= from+1
+			for _, n := range neighbors(p) {
+				stepCondition := steps[n.X][n.Y] > steps[p.X][p.Y]+1
 
-			_, marked := visited[n]
+				to, from := input[n.X][n.Y], input[p.X][p.Y]
+				heightCondition := to <= from+1
 
-			if stepCondition && heightCondition && !marked {
-				heap.Push(&queue, Point{X: n.X, Y: n.Y, Steps: p.Steps + 1})
+				if stepCondition && heightCondition && !visited[n] {
+					steps[n.X][n.Y] = steps[p.X][p.Y] + 1
+					queue2 = append(queue2, n)
+				}
 			}
 		}
+
+		queue1, queue2 = queue2, make([]Point, 0)
 	}
 
 	panic("unreachable")
 }
 
 func solveA(input []string) int {
-	queue := Heap(make([]Point, 0))
-	for x, line := range input {
-		if y := strings.Index(line, "S"); y != -1 {
-			heap.Push(&queue, Point{X: x, Y: y, Steps: 0})
-		}
-	}
-
 	return solve(input, func(c rune) bool {
 		return c == 'S'
 	})
