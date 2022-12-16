4fb871479e7605ff51353c23f84db8ad7dd75f4b
Andrey Bozhko <andybozhko@gmail.com>
2022-12-16 15:52:00 +0000

    update

--- day16/day16.go
+++ day16/day16.go
@@ -8,13 +8,16 @@ import (
 	"strings"
 )
 
-type Valve struct {
-	flow      int
-	neighbors []string
+type Progress struct {
+	elapsed  int
+	previous string
 }
 
-func solveA(input []string) int {
-	valves := make(map[string]Valve)
+func parseValves(input []string) (usefulValves map[string]int, dist map[string]map[string]int) {
+	valves := make(map[string]struct {
+		flow      int
+		neighbors []string
+	})
 
 	for _, line := range input {
 		if line == "" {
@@ -31,11 +34,14 @@ func solveA(input []string) int {
 
 		_ = utils.Must(fmt.Sscanf(part1, "Valve %s has flow rate=%d", &name, &flow))
 
-		valves[name] = Valve{flow, neighbors}
+		valves[name] = struct {
+			flow      int
+			neighbors []string
+		}{flow, neighbors}
 
 	}
 
-	dist := make(map[string]map[string]int)
+	dist = make(map[string]map[string]int)
 	for name, v := range valves {
 		dist[name] = make(map[string]int)
 		for nm := range valves {
@@ -59,122 +65,82 @@ func solveA(input []string) int {
 		}
 	}
 
-	usefulValves := make([]string, 0)
+	usefulValves = make(map[string]int)
 	for nm, v := range valves {
 		if v.flow > 0 {
-			usefulValves = append(usefulValves, nm)
+			usefulValves[nm] = v.flow
 		}
 	}
+
+	return
+}
+
+func solveA(input []string) int {
+	totalMinutes := 30
+	usefulValves, dist := parseValves(input)
 
 	seen := make(map[string]bool)
 	seen["AA"] = true
 
 	total := 0
-	var dfs func(released int, minutes int, prev string)
 
-	dfs = func(released int, minutes int, prev string) {
+	var dfs func(released int, progress Progress)
+
+	dfs = func(released int, progress Progress) {
 		if total < released {
 			total = released
 		}
-		for _, v := range usefulValves {
+		for v, flow := range usefulValves {
 			if seen[v] {
 				continue
 			}
 			seen[v] = true
 
-			newMinutes := minutes + dist[prev][v] + 1
-			if newMinutes < 30 {
-				dfs(released+valves[v].flow*(30-newMinutes), newMinutes, v)
+			newMinutes := progress.elapsed + dist[progress.previous][v] + 1
+			if newMinutes < totalMinutes {
+				dfs(released+flow*(totalMinutes-newMinutes), Progress{newMinutes, v})
 			}
 
 			delete(seen, v)
 		}
 	}
 
-	dfs(0, 0, "AA")
+	dfs(0, Progress{0, "AA"})
 
 	return total
 }
 
 func solveB(input []string) int {
-	valves := make(map[string]Valve)
-
-	for _, line := range input {
-		if line == "" {
-			continue
-		}
-
-		var flow int
-		var name string
-
-		line := strings.ReplaceAll(line, "valves", "valve")
-		part1, part2 := strings.Split(line, "; ")[0], strings.Split(line, "; ")[1]
-		part2 = strings.Split(part2, "valve ")[1]
-		neighbors := strings.Split(part2, ", ")
-
-		_ = utils.Must(fmt.Sscanf(part1, "Valve %s has flow rate=%d", &name, &flow))
-
-		valves[name] = Valve{flow, neighbors}
-
-	}
-
-	dist := make(map[string]map[string]int)
-	for name, v := range valves {
-		dist[name] = make(map[string]int)
-		for nm := range valves {
-			dist[name][nm] = math.MaxInt32
-		}
-
-		dist[name][name] = 0
-
-		for _, to := range v.neighbors {
-			dist[name][to] = 1
-		}
-	}
-
-	for k := range valves {
-		for i := range valves {
-			for j := range valves {
-				if dist[i][j] > dist[i][k]+dist[k][j] {
-					dist[i][j] = dist[i][k] + dist[k][j]
-				}
-			}
-		}
-	}
-
-	usefulValves := make([]string, 0)
-	for nm, v := range valves {
-		if v.flow > 0 {
-			usefulValves = append(usefulValves, nm)
-		}
-	}
+	totalMinutes := 26
+	usefulValves, dist := parseValves(input)
 
 	seen := make(map[string]bool)
 	seen["AA"] = true
 
 	total := 0
-	var dfs func(released int, minutes int, prev string, minutes2 int, prev2 string)
 
-	dfs = func(released int, minutes int, prev string, minutes2 int, prev2 string) {
+	var dfs func(released int, progress1, progress2 Progress)
+
+	dfs = func(released int, progress1, progress2 Progress) {
 		if total < released {
 			total = released
 		}
-		for _, v := range usefulValves {
+		for v, flow := range usefulValves {
 			if seen[v] {
 				continue
 			}
 			seen[v] = true
 
-			newMinutes := minutes + dist[prev][v] + 1
-			if newMinutes < 26 {
-				dfs(released+valves[v].flow*(26-newMinutes), minutes2, prev2, newMinutes, v)
+			newMinutes := progress1.elapsed + dist[progress1.previous][v] + 1
+			if newMinutes < totalMinutes {
+				dfs(released+flow*(totalMinutes-newMinutes), progress2, Progress{newMinutes, v})
 			}
 
 			delete(seen, v)
 		}
 	}
 
-	dfs(0, 0, "AA", 0, "AA")
+	dfs(0, Progress{0, "AA"}, Progress{0, "AA"})
 
 	return total
 }
