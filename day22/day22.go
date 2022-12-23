befbf7dcec7cef83bcfab34601641a4dd593f191
Andrey Bozhko <andybozhko@gmail.com>
2022-12-23 08:15:14 +0000

    add day 22 B

--- day22/day22.go
+++ day22/day22.go
@@ -38,28 +38,16 @@ func (d *direction) TurnLeft() {
 
 type Map [][]cell
 
-func (pos Point) Next(dir direction, m Map) Point {
-	maxX, maxY := len(m[0]), len(m)
+type wrappingFunc func(p Point, dir direction, m Map) (Point, direction)
 
-	step := directions[int(dir)]
-
-	x, y := pos.X, pos.Y
-
-	if step.Y == 0 {
-		x = (pos.X + step.X + maxX) % maxX
-		for m[pos.Y][x] == null {
-			x = (x + step.X + maxX) % maxX
-		}
+func (m Map) At(p Point) cell {
+	if p.Y < 0 || p.Y >= len(m) {
+		return null
 	}
-
-	if step.X == 0 {
-		y = (pos.Y + step.Y + maxY) % maxY
-		for m[y][pos.X] == null {
-			y = (y + step.Y + maxY) % maxY
-		}
+	if p.X < 0 || p.X >= len(m[p.Y]) {
+		return null
 	}
-
-	return Point{x, y}
+	return m[p.Y][p.X]
 }
 
 func parseInput(input []string) (m Map, start *Point, dir direction, inst string) {
@@ -103,7 +91,7 @@ func parseInput(input []string) (m Map, start *Point, dir direction, inst string
 	return
 }
 
-func solve(input []string, wrap func(p Point, dir direction, m Map) Point) int {
+func solve(input []string, wrap wrappingFunc) int {
 	grid, pos, dir, instr := parseInput(input)
 
 	for instr != "" {
@@ -119,19 +107,22 @@ func solve(input []string, wrap func(p Point, dir direction, m Map) Point) int {
 			}
 			steps := utils.Must(strconv.Atoi(instr[:idx]))
 
-			dxdy := directions[int(dir)]
-
 			var p Point
+			var d direction
+
 			for s := 0; s < steps; s++ {
-				p = *pos
+				p, d = *pos, dir
+				dxdy := directions[int(dir)]
+
 				p.X, p.Y = p.X+dxdy.X, p.Y+dxdy.Y
-				if grid[p.Y][p.X] == null {
-					p = wrap(*pos, dir, grid)
+				if grid.At(p) == null {
+					p, d = wrap(*pos, d, grid)
 				}
-				if grid[p.Y][p.X] == wall {
+				if grid.At(p) == wall {
 					break
 				}
 				*pos = p
+				dir = d
 			}
 
 			instr = instr[idx:]
@@ -142,7 +133,7 @@ func solve(input []string, wrap func(p Point, dir direction, m Map) Point) int {
 }
 
 func solveA(input []string) int {
-	return solve(input, func(pos Point, dir direction, m Map) Point {
+	return solve(input, func(pos Point, dir direction, m Map) (Point, direction) {
 		maxX, maxY := len(m[0]), len(m)
 		step := directions[int(dir)]
 		x, y := pos.X, pos.Y
@@ -161,13 +152,81 @@ func solveA(input []string) int {
 			}
 		}
 
-		return Point{x, y}
+		return Point{x, y}, dir
 	})
 }
 
 func solveB(input []string) int {
+	sz := 50
+	return solve(input, func(p Point, dir direction, m Map) (next Point, d direction) {
+		faces := [][]int{{0, 1, 2}, {0, 3, 0}, {4, 5, 0}, {6, 0, 0}}
+		f := faces[p.Y/sz][p.X/sz]
 
-	return 0
+		switch f {
+		case 1:
+			switch dir {
+			case L: // -> 4
+				next, d = Point{0, 3*sz - 1 - p.Y}, R
+			case U: // -> 6
+				next, d = Point{0, p.X + 2*sz}, R
+			default:
+				panic("should not happen")
+			}
+		case 2:
+			switch dir {
+			case R: // -> 5
+				next, d = Point{2*sz - 1, 3*sz - 1 - p.Y}, L
+			case D: // -> 3
+				next, d = Point{2*sz - 1, p.X - sz}, L
+			case U: // -> 6
+				next, d = Point{p.X - 2*sz, 4*sz - 1}, U
+			default:
+				panic("should not happen")
+			}
+		case 3:
+			switch dir {
+			case R: // -> 2
+				next, d = Point{p.Y + sz, sz - 1}, U
+			case L: // -> 4
+				next, d = Point{p.Y - sz, 2 * sz}, D
+			default:
+				panic("should not happen")
+			}
+		case 4:
+			switch dir {
+			case L: // -> 1
+				next, d = Point{sz, 3*sz - 1 - p.Y}, R
+			case U: // -> 3
+				next, d = Point{sz, p.X + sz}, R
+			default:
+				panic("should not happen")
+			}
+		case 5:
+			switch dir {
+			case R: // -> 2
+				next, d = Point{3*sz - 1, 3*sz - 1 - p.Y}, L
+			case D: // -> 6
+				next, d = Point{sz - 1, p.X + 2*sz}, L
+			default:
+				panic("should not happen")
+			}
+		case 6:
+			switch dir {
+			case R: // -> 5
+				next, d = Point{p.Y - 2*sz, 3*sz - 1}, U
+			case D: // -> 2
+				next, d = Point{p.X + 2*sz, 0}, D
+			case L: // -> 1
+				next, d = Point{p.Y - 2*sz, 0}, D
+			default:
+				panic("should not happen")
+			}
+		default:
+			panic("unreachable")
+		}
+
+		return
+	})
 }
 
 func main() {
--- day22/day22_test.go
+++ day22/day22_test.go
@@ -31,5 +31,74 @@ func TestA(t *testing.T) {
 }
 
 func TestB(t *testing.T) {
-	assert.Equal(t, 5031, solveB(input))
+	sz := 4
+	assert.Equal(t, 5031, solve(input, func(p Point, dir direction, m Map) (next Point, d direction) {
+		faces := [][]int{{0, 0, 1, 0}, {2, 3, 4, 0}, {0, 0, 5, 6}}
+		f := faces[p.Y/sz][p.X/sz]
+
+		switch f {
+		case 1:
+			switch dir {
+			case R: // -> 6
+				next, d = Point{4*sz - 1, 3*sz - p.Y - 1}, L
+			case L: // -> 3
+				next, d = Point{p.Y + sz, sz}, D
+			case U: // -> 2
+				next, d = Point{3*sz - p.X - 1, sz}, D
+			default:
+				panic("should not happen")
+			}
+		case 2:
+			switch dir {
+			case D: // -> 5
+				next, d = Point{3*sz - p.X - 1, 3*sz - 1}, U
+			case L: // -> 6
+				next, d = Point{5*sz - p.Y - 1, 3*sz - 1}, U
+			case U: // -> 1
+				next, d = Point{3*sz - p.X - 1, 0}, D
+			default:
+				panic("should not happen")
+			}
+		case 3:
+			switch dir {
+			case D: // -> 5
+				next, d = Point{2*sz - 1, 4*sz - p.X - 1}, R
+			case U: // -> 1
+				next, d = Point{2 * sz, p.X - sz}, R
+			default:
+				panic("should not happen")
+			}
+		case 4:
+			switch dir {
+			case R: // -> 6
+				next, d = Point{5*sz - p.Y - 1, 2 * sz}, D
+			default:
+				panic("should not happen")
+			}
+		case 5:
+			switch dir {
+			case D: // -> 2
+				next, d = Point{3*sz - p.X - 1, 2*sz - 1}, U
+			case L: // -> 3
+				next, d = Point{4*sz - p.Y - 1, 2*sz - 1}, U
+			default:
+				panic("should not happen")
+			}
+		case 6:
+			switch dir {
+			case R: // -> 1
+				next, d = Point{3*sz - 1, 3*sz - p.Y - 1}, L
+			case D: // -> 2
+				next, d = Point{0, 5*sz - p.X - 1}, R
+			case U: // -> 4
+				next, d = Point{3*sz - 1, 5*sz - p.X - 1}, L
+			default:
+				panic("should not happen")
+			}
+		default:
+			panic("unreachable")
+		}
+
+		return
+	}))
 }
